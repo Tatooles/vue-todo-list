42e3013e7862084662f44949ee28646bf8a4e308
Kevin Tatooles <tatooles@wisc.edu>
2022-11-29 22:54:09 -0600

    Implement mvp functionality

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -3,18 +3,35 @@ import { ref } from 'vue'
 
 let id = 0
 
+const currentText = ref('')
+
 const todos = ref([
   { id: id++, text: 'example todo' }
 ])
+
+const addItem = () => {
+  console.log('submit form')
+  todos.value.push({ id: id++, text: currentText.value })
+  currentText.value = ''
+}
+
+const removeItem = (todo: any) => {
+  todos.value = todos.value.filter(item => item.text != todo.text)
+}
 </script>
 
 <template>
   <div class="card">
     <h2>Enter your todo items below:</h2>
+    <form @submit.prevent="addItem">
+      <input v-model="currentText">
+      <button>Add Item</button>
+    </form>
 
     <ul>
       <li v-for="todo in todos" :key="todo.id">
         {{ todo.text }}
+        <button @click="removeItem(todo)">X</button>
       </li>
     </ul>
   </div>
