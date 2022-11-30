33b476c29bacdcf93944470b89463c5ce0e1ecd9
Kevin Tatooles <tatooles@wisc.edu>
2022-11-29 22:31:39 -0600

    Create button to hide boilerplate

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,18 @@
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
 import HelloWorld from './components/HelloWorld.vue'
+import { ref } from 'vue';
+
+const showHome = ref(true)
+
+const hideHome = () => {
+  showHome.value = false;
+}
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" msg2="second message" />
+  <button v-if="showHome" @click="hideHome">Hide Home Screen</button>
+  <HelloWorld v-if="showHome" msg="Vite + Vue" msg2="second message" />
 </template>
 
 <style scoped>
