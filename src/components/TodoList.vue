e7e074577490eb5b1e80c045ef79bbcd3a532ac9
Kevin Tatooles <tatooles@wisc.edu>
2023-01-08 22:28:55 -0600

    UI for adding an item

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -18,6 +18,10 @@ const removeItem = (todo: any) => {
   todos.value = todos.value.filter(t => t !== todo)
 }
 
+const completeItem = (todo: any) => {
+  // Move item to other list
+}
+
 const sortList = () => {
   todos.value.sort((a, b) => {
     if (a.text < b.text) {
@@ -44,15 +48,27 @@ const reverseList = () => {
     </form>
 
     <ul>
-      <li class="flex justify-between mt-2 hover:bg-gray-400 rounded-md pl-2 align-middle" v-for="todo in todos"
-        :key="todo.id">
+      <li class="flex justify-between mt-2 hover:bg-gray-400 rounded-md pl-2 align-middle border-gray-600 border-2"
+        v-for="todo in todos" :key="todo.id">
         {{ todo.text }}
-        <button class="bg-red-600 p-1 rounded-md" @click="removeItem(todo)">Delete</button>
+        <div>
+          <button class="bg-green-500 p-1 rounded-md mr-1" @click="completeItem(todo)">Done</button>
+          <button class="bg-red-600 p-1 rounded-md" @click="removeItem(todo)">Delete</button>
+        </div>
       </li>
     </ul>
 
     <button @click="sortList" class="bg-gray-500 rounded-md mt-8 mb-2">Sort list</button>
     <button @click="reverseList" class="bg-gray-500 rounded-md">Reverse Order</button>
+
+    <div class="mt-3">
+      <h2>Completed items</h2>
+      <ul>
+        <li>
+          test item
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
